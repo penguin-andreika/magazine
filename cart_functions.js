5623e92892cf8944f07aa7dcbd5f8923fc28b7ff
George <george.postoronca@gmail.com>
2021-11-25 00:14:48 +0300

    upd

--- cart_functions.js
+++ cart_functions.js
@@ -4,17 +4,38 @@ let totalEl = document.querySelector("#total");
 let totalAmountEl = document.querySelector("#total_amount");
 let gol = document.querySelector("#gol");
 let min = document.querySelector("#min");
+let cartItems = document.querySelector("#cart-items");
 
+function addInCart(name = "", price = 0, amount = 1) {
+    let element = document.createElement("div");
+    let nameEl = document.createElement("h3");
+    let priceEl = document.createElement("em");
+    let amountEl = document.createElement("span");
+
+    nameEl.innerText = name;
+    priceEl.innerText = "Price: " + price;
+    amountEl.innerText = "Amount: " + amount;
+
+    element.append(nameEl);
+    element.append(priceEl);
+    element.append(amountEl);
+    cartItems.append(element)
+}
 
 function calc() {
     console.log(total)
-    // let price = 0;
-    // total.forEach(item => price += Number(item))
-    // totalEl.style.display = "block";
-    // totalAmountEl.innerText = price;
+    let price = 0;
+    cartItems.innerHTML = "";
+    total.forEach(item => {
+        addInCart(item.name, item.price, item.amount);
+    });
 
-    // if(price > 0) gol.style.display = "none";
-    // if(price > 100) min.style.display = "none";
+    total.forEach(item => price += Number(item.price))
+    totalEl.style.display = "block";
+    totalAmountEl.innerText = price;
+
+    if(price > 0) gol.style.display = "none";
+    if(price > 100) min.style.display = "none";
 }
 
 buttons.forEach((item) => {
@@ -22,24 +43,10 @@ buttons.forEach((item) => {
         let price = event.target.dataset.pret;
         let name = event.target.dataset.name;
 
-        if(!total.length) {
-            console.log(total.length)
-            total.push({
-                name: name,
-                amount: 1,
-                price: price
-            });
-            calc();
-            return false;
-        }
-
-        total = total.map(item => {
-            console.log("mao: ", item)
-            return item.name == name ? item.amount += 1 : {
-                name: name,
-                amount: 1,
-                price: price
-            }
+        total.push({
+            name: name,
+            amount: 1,
+            price: price
         });
 
         calc();
